0639a45df64c31a73e713cc5c66d585bdb390cf0
Davida Pitts <Davida@orangemooncreative.com>
2018-10-29 21:55:50 +0800

    Add markers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { MAP_API_KEY } from './credentials';
+import { searchVenues } from './api';
 import './App.css';
 
 // load google maps script
@@ -14,22 +15,60 @@ function loadJS(url) {
 }
 
 class App extends Component {
-  // load the map and bind initMap function to the window object
-  componentDidMount = () => {
-    this.loadMap();
-    window.initMap = this.initMap;
+  state = {
+    venues: [],
   };
 
+  // get the venues from the api
+  componentDidMount = () => {
+    this.getVenues();
+  };
+
+  // load the google maps script
   loadMap = () => {
     loadJS(
       `https://maps.googleapis.com/maps/api/js?key=${MAP_API_KEY}&callback=initMap`
     );
+    // connect the initMap() function to the global window context, so google maps can invoke it
+    window.initMap = this.initMap;
+  };
+
+  // make api call and get the venues based on submitted parameters
+  // map over the response (venues) and update the marker state
+  // asynchronously load the Google Maps script
+  getVenues = () => {
+    searchVenues({
+      near: 'Ubud',
+      query: 'food',
+      limit: 10,
+    })
+      .then(res => {
+        const { venues } = res.data.response;
+        this.setState({ venues }, this.loadMap());
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
+      center: { lat: -8.509921, lng: 115.269213 },
+      zoom: 15,
+    });
+
+    // map over the venues and display individual markers based on their lat and lng values
+    const { venues } = this.state;
+    venues.map(v => {
+      const contentString = `${v.name}`;
+      const infowindow = new window.google.maps.InfoWindow({
+        content: contentString,
+      });
+      const marker = new window.google.maps.Marker({
+        position: { lat: v.location.lat, lng: v.location.lng },
+        map,
+      });
+      return marker;
     });
   };
 
---
+++ src/api.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import { SQUARE_CLIENT_ID, SQUARE_CLIENT_SECRET } from './credentials';
+
+// TODO: Figure out which functions do not need to be exported and remove export
+// TODO: Add error handling for the functions (specifically the api calls)
+
+export function mapBase() {
+  return 'https://maps.googleapis.com/maps/api/js?v=3.exp&';
+}
+
+export function squareBase() {
+  return 'https://api.foursquare.com/v2/venues/';
+}
+
+// takes in the required keys for the API request
+// returns them assembled for the API request
+// client_id=myClientID&client_secret=myClientSecret&v=20180910
+export function squareAuth() {
+  const keys = {
+    client_id: SQUARE_CLIENT_ID,
+    client_secret: SQUARE_CLIENT_SECRET,
+    v: 20180910,
+  };
+  return Object.keys(keys)
+    .map(k => `${k}=${keys[k]}`)
+    .join('&');
+}
+
+// simple fetch api with no parameters included in the request
+// will be used to fetch photos for the venue
+export function squareSimpleFetch(endpoint) {
+  return axios.get(`${squareBase()}${endpoint}?${squareAuth()}`);
+}
+
+// returns the params set for the search function
+// i.e near=Ubud&query=food&limit=10
+export function squareParams(urlParams) {
+  return Object.keys(urlParams)
+    .map(k => `${k}=${urlParams[k]}`)
+    .join('&');
+}
+
+// api call with parameters included in the request
+// i.e https://api.foursquare.com/v2/venues/search?client_id=myClientID&client_secret=myClientSecret&v=20180910&near=Ubud&query=food&limit=10
+export function squareParamsFetch(endpoint, urlParams) {
+  return axios.get(
+    `${squareBase()}${endpoint}?${squareAuth()}&${squareParams(urlParams)}`
+  );
+}
+
+// returns a list of venues near the current location
+export function searchVenues(urlParams) {
+  return squareParamsFetch('search', urlParams);
+}
+
+// returns photos for a specific venue
+export function getPhotos(VENUE_ID) {
+  return squareSimpleFetch(`${VENUE_ID}/photos`);
+}
